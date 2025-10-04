fcba70756159f1fae3dedbd5c302926504ae8de5
rpyadav3037-source <rpyadav.3037@gmail.com>
2025-10-04 20:28:29 +0530

    Update folder_paths.py

--- folder_paths.py
+++ folder_paths.py
@@ -1,6 +1,4 @@
-movie_paths={"man":r"D:\Movies\Movies\MCU\6_Iron Man 3 (2013) 720p Hindi English Vegamovie.NL.mkv",
-             "thor":r"D:\Movies\Movies\MCU\14_Thor Ragnarok (2017) Dual Audio {Hindi-English} 720p BluRay 1.1GB [Vegamovies.NL].mkv",
-             "caption":r"D:\Movies\Movies\MCU\4_Captain America- First Avenger 2011 720p Hindi English Vegamovies.NL.mkv",
-             "doctor":r"D:\Movies\Movies\MCU\10_Doctor.Strange.2016.1080P.Bluray.Hindi.English.Esubs.Vegamovies.is.mkv"
+movie_paths={"Moive name (in lowercase)":"Location of movie stored on your device"}
              
+
              }
