6baaeb9955beccd69cc2f0cf93d740ced7b9511e
rpyadav3037-source <rpyadav.3037@gmail.com>
2025-10-04 20:26:37 +0530

    Update MusicLibrary.py

--- MusicLibrary.py
+++ MusicLibrary.py
@@ -1,10 +1,11 @@
 music={"sun":"https://youtu.be/H2f7MZaw3Yo?si=WZCO2v18tLubK8hP",
-        "rahul":"https://youtu.be/FJ55SHCzt88?si=I7W1diCC1ASgYmQ5",
+        "earth":"https://youtu.be/FJ55SHCzt88?si=I7W1diCC1ASgYmQ5",
         "moon":"https://youtu.be/b4hrUSBP4nc?si=PA45wNOViHWYe7v1",  
         "tree":"https://youtu.be/xRb8hxwN5zc?si=f3oreqIg-iEZRWE_",    
         "water":"https://youtu.be/DAYszemgPxc?si=Vl8vOKl7vgmrkUE8",    
         "fan":"https://youtu.be/bdS6OoH1W2A?si=YMfsQe2bMvpyaUeu",   
          }
+
 
 
 
